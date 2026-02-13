b1b544408c4a10c4a1430ba27b1225ea6c6c0fff
Irfan Muneer <Irfaanpashamuunir@gmail.com>
2026-02-13 22:22:59 +0400

    infra: implement GitHub Actions CI/CD pipeline with headless support

--- ecommerce-demoblaze/demoblaze-tests/src/main/java/com/irfan/ecommerce/base/DriverFactory.java
+++ ecommerce-demoblaze/demoblaze-tests/src/main/java/com/irfan/ecommerce/base/DriverFactory.java
@@ -4,24 +4,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DriverFactory {
     // 1. ThreadLocal container to ensure thread-safety
     private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+     String headless = System.getProperty("headless");
 
     public static WebDriver initDriver(String browser) {
-        System.out.println("LOG: Initializing " + browser + " for Thread: " + Thread.currentThread().getId());
+    // FIX: Declare 'headless' as a local String variable inside the static method
+    String headless = System.getProperty("headless"); 
+    
+    if (browser.equalsIgnoreCase("chrome")) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
         
-        if (browser.equalsIgnoreCase("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            // Headless is great for CI/CD pipelines
-            // options.addArguments("--headless"); 
-            tlDriver.set(new ChromeDriver(options));
+        // Logical check: If Maven sends -Dheadless=true, we add the arguments
+        if (headless != null && headless.equalsIgnoreCase("true")) {
+            options.addArguments("--headless=new"); // For Chrome 109+
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-gpu");
         }
         
-        getDriver().manage().window().maximize();
-        getDriver().manage().deleteAllCookies();
-        return getDriver();
+        tlDriver.set(new ChromeDriver(options));
     }
+    
+    getDriver().manage().window().maximize();
+    return getDriver();
+}
 
     // 2. Public getter to retrieve the driver for the SPECIFIC thread
     public static synchronized WebDriver getDriver() {
